5cc3b7759b212acb31b7116052ac56b8167f9119
zlwym <119268153+zlwym@users.noreply.github.com>
2026-07-27 16:40:04 -0700

    Delete iat461_a3_partb_zoemann_301452821 directory

--- iat461_a3_partb_zoemann_301452821/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import geopandas as gpd
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-
-gdf = gpd.read_file("business-licences.geojson")
